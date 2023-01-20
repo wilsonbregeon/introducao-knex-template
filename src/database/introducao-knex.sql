63fe7d5dde86265cce5e81a304e695032ef56a61
yuzo <yuzokamoto@gmail.com>
2023-01-20 08:47:08 -0300

    update

--- src/database/introducao-knex.sql
+++ src/database/introducao-knex.sql
@@ -11,17 +11,3 @@ CREATE TABLE songs (
     band_id TEXT NOT NULL,
     FOREIGN KEY (band_id) REFERENCES bands (id)
 );
-
-CREATE TABLE albums (
-    id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    band_id TEXT NOT NULL,
-    FOREIGN KEY (band_id) REFERENCES bands (id)
-);
-
-CREATE TABLE albums_songs (
-    album_id TEXT NOT NULL,
-    song_id TEXT NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES albums (id),
-    FOREIGN KEY (song_id) REFERENCES songs (id)
-);
